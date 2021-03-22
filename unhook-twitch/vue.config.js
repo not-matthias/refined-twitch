4358b4405ad136dd4ff24a5678d1311d5a4d53df
not-matthias <26800596+not-matthias@users.noreply.github.com>
2021-03-22 16:36:17 +0100

    Added the content and background scripts

--- unhook-twitch/vue.config.js
+++ unhook-twitch/vue.config.js
@@ -2,10 +2,10 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
 // Generate pages object
+//
+
 const pagesObj = {};
-
 const chromeName = ["popup", "options"];
-
 chromeName.forEach(name => {
   pagesObj[name] = {
     entry: `src/${name}/index.ts`,
@@ -14,24 +14,50 @@ chromeName.forEach(name => {
   };
 });
 
-const plugins =
-  process.env.NODE_ENV === "production"
-    ? [
-        {
-          from: path.resolve("src/manifest.production.json"),
-          to: `${path.resolve("dist")}/manifest.json`
-        }
-      ]
-    : [
-        {
-          from: path.resolve("src/manifest.development.json"),
-          to: `${path.resolve("dist")}/manifest.json`
-        }
-      ];
+
+// Copy the manifest
+//
+const plugins = [
+  process.env.NODE_ENV === "production" ?
+    {
+      from: path.resolve("src/manifest.production.json"),
+      to: `${path.resolve("dist")}/manifest.json`
+    }
+    :
+    {
+      from: path.resolve("src/manifest.development.json"),
+      to: `${path.resolve("dist")}/manifest.json`
+    }
+];
 
 module.exports = {
+  filenameHashing: false,
   pages: pagesObj,
   configureWebpack: {
+    entry: {
+      background: './src/background/index.ts',
+      content: './src/content/index.ts',
+    },
+    resolve: {
+      extensions: ['.ts'],
+    },
+    // loaders
+    module: {
+      rules: [
+        {
+          test: /\.ts?/,
+          exclude: /(options|popup)/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                happyPackMode: true // https://stackoverflow.com/a/53411833
+              }
+            }
+          ]
+        }
+      ]
+    },
     plugins: [CopyWebpackPlugin(plugins)]
-  }
+  },
 };

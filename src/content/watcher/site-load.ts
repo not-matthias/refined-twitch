a6e03d43de8efc7f30db213f90a720547d2d833c
not-matthias <26800596+not-matthias@users.noreply.github.com>
2021-07-04 15:35:19 +0200

    Removed an unneeded variable

--- src/content/watcher/site-load.ts
+++ src/content/watcher/site-load.ts
@@ -1,8 +1,6 @@
 import { EventEmitter, Listener } from "events";
 
 class SiteLoadWatcher extends EventEmitter {
-    private _listenerCount = 0;
-
     onLoaded(listener: Listener) {
         this.on("site-loaded", listener);
     }
